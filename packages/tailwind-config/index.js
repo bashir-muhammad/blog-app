cbbf948279bf4fa9f0e6207a37b6ec8d190fd6d3
karimim <muhammad.karimi@post.ch>
2026-02-28 01:50:05 +0100

    chore: clean-up tokens

--- packages/tailwind-config/index.js
+++ packages/tailwind-config/index.js
@@ -2,30 +2,6 @@
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  // Content is defined in each consuming app for proper tree-shaking.
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: "var(--font-family-sans)",
-        mono: "var(--font-family-mono)",
-      },
-      colors: {
-        border: "var(--border)",
-        input: "var(--input)",
-        ring: "var(--ring)",
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: {
-          DEFAULT: "var(--primary)",
-          foreground: "var(--primary-foreground)",
-        },
-        secondary: {
-          DEFAULT: "var(--secondary)",
-          foreground: "var(--secondary-foreground)",
-        },
-      },
-    },
-  },
   plugins: [
     plugin(function ({ addBase, addComponents }) {
       addBase({});
