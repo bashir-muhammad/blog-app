84d4dd4bd543597eec4fa6fb5d42d29eaabc73eb
karimim <muhammad.karimi@post.ch>
2026-03-08 14:18:24 +0100

    chore: adjust spaces

--- packages/modules/src/modules/footer/footer.tsx
+++ packages/modules/src/modules/footer/footer.tsx
@@ -1,7 +1,10 @@
+import { SectionContent } from "@repo/ui/section";
 const Footer = () => {
   return (
-    <footer className="container mx-auto border-t bg-white py-4 text-center text-sm text-gray-500">
-      <p>&copy; {new Date().getFullYear()} My Blog. All rights reserved.</p>
+    <footer className="text-dark-alt mx-auto mt-[var(--spacing-section-md)] border-t bg-white py-4 text-center text-sm">
+      <SectionContent>
+        <p>&copy; {new Date().getFullYear()} My Blog. All rights reserved.</p>
+      </SectionContent>
     </footer>
   );
 };
